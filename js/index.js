d874cb9efaf05e119781088775c1ac9e14085e55
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-01-12 16:56:11 +0100

    Modernize things.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 import '../sass/main.scss' // stupid.
+
+const $burger = document.querySelector('#toggle-nav')
+const $nav = document.querySelector('#main-nav')
+
+$burger.onclick = function() {
+  $burger.classList.toggle('is-active')
+  $nav.classList.toggle('is-active')
+}
